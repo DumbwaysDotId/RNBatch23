c37d86fe169d97460e11ef0d07f202d3f04ed5a5
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-06-30 11:07:19 +0700

    Native Base

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { NativeBaseProvider, Text, Box } from "native-base";
 import Home from "./src/screens/Home";
 import Counter from "./src/screens/Counter";
+import Template from "./src/screens/Template";
+import Thema from "./src/screens/Theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-      {/* <Counter /> */}
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        {/* <Home /> */}
+        {/* <Template /> */}
+        <Thema />
+        {/* <Counter /> */}
+        <StatusBar style="auto" />
+      </Box>
+    </NativeBaseProvider>
   );
 }
 
---
+++ src/screens/Template.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FormControl, Input, Stack } from "native-base";
+
+function Template() {
+  return (
+    <FormControl isRequired isInvalid>
+      <Stack mx={4}>
+        <FormControl.Label>Favorite framework</FormControl.Label>
+        <Input p={2} placeholder="Is it react?" />
+        <FormControl.HelperText>
+          We'll keep this between us.
+        </FormControl.HelperText>
+        <FormControl.ErrorMessage>Something is wrong.</FormControl.ErrorMessage>
+      </Stack>
+    </FormControl>
+  );
+}
+
+export default Template;
